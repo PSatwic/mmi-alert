1dc7601074e514a067b6dd8af4598e440557f3ba
satwic p <123649710+PSatwic@users.noreply.github.com>
2023-02-02 18:04:07 +0000

    1. changed scraping mechanism to beautyfulsoup from selenium time and processing power reduced 2. added emojis for easy recognition 3. changed the if clause condition for LB alert and UB alert by added old = new and removing from main if clause mmi<target

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
 from keep_alive import keep_alive
-from selenium import webdriver
+from bs4 import BeautifulSoup
 import datetime
 import pytz
 import time
 import requests
-
-def start_driver():
-  options = webdriver.ChromeOptions()
-  options.add_argument("disable-infobars")
-  options.add_argument("start-maximized")
-  options.add_argument("disable-dev-shm-usage")
-  options.add_argument("no-sandbox")
-  options.add_experimental_option("excludeSwitches", ["enable-automation"])
-  options.add_argument("disable-blink-features=AutomationControlled")
-  #Starting driver
-  driver = webdriver.Chrome(options=options)
-  driver.get("https://www.tickertape.in/market-mood-index")
-  return driver
+import random
 
 def today_now():
   #Creates a file name with local india time
   India_local_time = datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
   time_now = India_local_time.strftime("%Y-%m-%d %H:%M:%S")
-  return [time_now,India_local_time]
+  return [time_now, India_local_time]
+
 
 def send_msg(text):
   TOKEN = "6165499133:AAEvdzvgwhTi13lfDEMSIXjmiaJTp5fC3uI"
@@ -32,58 +21,62 @@ def send_msg(text):
   requests.get(url)
   return
 
-def get_mmi(driver):
-  time.sleep(2) #wait to load the data
-  element = driver.find_element(by="xpath",value="/html/body/div/div[3]/div/div[1]/div[1]/div/div[2]/span")
-  return float(element.text)
+
+def get_mmi():
+  url = "https://www.tickertape.in/market-mood-index"
+  content = requests.get(url).text
+  soup = BeautifulSoup(content, 'html.parser')
+  mmi = soup.find("span" , class_="jsx-1637443598 jsx-1556920289 number").get_text()
+  return float(mmi)
 
 
 def main():
-  driver = start_driver()
   run_no = 0
-  new_mmi = get_mmi(driver)
+  new_mmi = get_mmi()
   old_mmi = new_mmi
   in_id = 0
   out_id = 0
   while True:
-    while today_now()[1].hour < 16 and today_now()[1].hour >= 9:
+    while today_now()[1].hour < 24 and today_now()[1].hour >= 9:
       print(f"run_no  {run_no}")
       print(f"new mmi is {new_mmi}")
       print(f"old mmi is {old_mmi}")
-      lower_bound = old_mmi-delta_change
+      lower_bound = old_mmi - delta_change
       print(f"Range lower bound is {lower_bound}")
-      upper_bound = old_mmi+delta_change
+      upper_bound = old_mmi + delta_change
       print(f"Range upper bound is {upper_bound}")
-  
-      if(new_mmi <= target_value):
-        if(new_mmi<=lower_bound):
-          send_msg(f"mmi reduced more than {delta_change} and value is {new_mmi}")
-        elif (new_mmi>=upper_bound):
-          send_msg(f"mmi increased more than {delta_change} and value is {new_mmi}")
-        elif(in_id == 0):
-          send_msg(f"mmi is lower than {target_value} and value is {new_mmi}")
-        old_mmi = new_mmi
+
+      if (new_mmi <= target_value):
+        if (new_mmi <= lower_bound):
+          send_msg(u"\u2705"+f"mmi reduced more than {delta_change} and value is {new_mmi}")
+          old_mmi = new_mmi
+        elif (new_mmi >= upper_bound):
+          send_msg(u"\u274C"+f"&#10060 mmi increased more than {delta_change} and value is {new_mmi}")
+          old_mmi = new_mmi
+        elif (in_id == 0):
+          send_msg(u"\u2705"+f"mmi is lower than {target_value} and value is {new_mmi}")
         in_id = 1
         out_id = 0
       elif (out_id == 0):
-        send_msg(f"mmi is more than {target_value} and value is {new_mmi}")
+        send_msg(u"\u274C"+f"mmi is more than {target_value} and value is {new_mmi}")
         in_id = 0
         out_id = 1
         old_mmi = new_mmi
-        
-      time.sleep(refresh_time*60)
-      driver.get(driver.current_url)  # refreshes the page
-      new_mmi = get_mmi(driver) #assigns the refreshed value
-      run_no = run_no+1
-    if(today_now()[1].weekday() < 5 ):
+
+      time.sleep(refresh_time * 60)
+      new_mmi = get_mmi()  #assigns the refreshed value
+      run_no = run_no + 1
+    if (today_now()[1].weekday() < 5):
+      print("sleeping for 17 hrs")
       time.sleep(61200)
     else:
+      print("sleeping for weekend")
       time.sleep(838800)
   return
 
+
 keep_alive()
-delta_change = 0.1
-refresh_time = 3.0 # in minutes
+delta_change = 5.0
+refresh_time = 3.0  # in minutes
 target_value = 30.0
 main()
-
