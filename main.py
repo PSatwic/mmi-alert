856dcf6a41bd515210ecb1bdc4d033fcbe9a6122
satwic p <123649710+PSatwic@users.noreply.github.com>
2023-02-02 11:16:54 +0000

    Added sleep for week end and non working hours

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# from keep_alive import keep_alive
+from keep_alive import keep_alive
 from selenium import webdriver
 import datetime
 import pytz
@@ -46,37 +46,42 @@ def main():
   in_id = 0
   out_id = 0
   while True:
-    print(f"run_no  {run_no}")
-    print(f"new mmi is {new_mmi}")
-    print(f"old mmi is {old_mmi}")
-    lower_bound = old_mmi-delta_change
-    print(f"Range lower bound is {lower_bound}")
-    upper_bound = old_mmi+delta_change
-    print(f"Range upper bound is {upper_bound}")
-
-    if(new_mmi <= target_value):
-      if(new_mmi<=lower_bound):
-        send_msg(f"mmi reduced more than {delta_change} and value is {new_mmi}")
-      elif (new_mmi>=upper_bound):
-        send_msg(f"mmi increased more than {delta_change} and value is {new_mmi}")
-      elif(in_id == 0):
-        send_msg(f"mmi is lower than {target_value} and value is {new_mmi}")
-      old_mmi = new_mmi
-      in_id = 1
-      out_id = 0
-    elif (out_id == 0):
-      send_msg(f"mmi is more than {target_value} and value is {new_mmi}")
-      in_id = 0
-      out_id = 1
-      old_mmi = new_mmi
-      
-    time.sleep(refresh_time*60)
-    driver.get(driver.current_url)  # refreshes the page
-    new_mmi = get_mmi(driver) #assigns the refreshed value
-    run_no = run_no+1
+    while today_now()[1].hour < 16 and today_now()[1].hour >= 9:
+      print(f"run_no  {run_no}")
+      print(f"new mmi is {new_mmi}")
+      print(f"old mmi is {old_mmi}")
+      lower_bound = old_mmi-delta_change
+      print(f"Range lower bound is {lower_bound}")
+      upper_bound = old_mmi+delta_change
+      print(f"Range upper bound is {upper_bound}")
+  
+      if(new_mmi <= target_value):
+        if(new_mmi<=lower_bound):
+          send_msg(f"mmi reduced more than {delta_change} and value is {new_mmi}")
+        elif (new_mmi>=upper_bound):
+          send_msg(f"mmi increased more than {delta_change} and value is {new_mmi}")
+        elif(in_id == 0):
+          send_msg(f"mmi is lower than {target_value} and value is {new_mmi}")
+        old_mmi = new_mmi
+        in_id = 1
+        out_id = 0
+      elif (out_id == 0):
+        send_msg(f"mmi is more than {target_value} and value is {new_mmi}")
+        in_id = 0
+        out_id = 1
+        old_mmi = new_mmi
+        
+      time.sleep(refresh_time*60)
+      driver.get(driver.current_url)  # refreshes the page
+      new_mmi = get_mmi(driver) #assigns the refreshed value
+      run_no = run_no+1
+  if(today_now()[1].weekday() < 5 ):
+    time.sleep(61200)
+  else:
+    time.sleep(838800)
   return
 
-# keep_alive()
+keep_alive()
 delta_change = 0.1
 refresh_time = 3.0 # in minutes
 target_value = 30.0
