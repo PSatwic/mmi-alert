66e88863971a3178823f0169cd6a15be55356a3b
satwic p <123649710+PSatwic@users.noreply.github.com>
2023-02-11 10:20:10 +0000

    added telebot api to get chat ids of people joined and rearranged the if  conditions

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
+from replit import db
 from keep_alive import keep_alive
 from bs4 import BeautifulSoup
 import datetime
 import pytz
 import time
 import requests
-import random
 
+def get_chatid():
+  url = "https://telegrambotdb.satwicp.repl.co/api/xyz"
+  content = requests.get(url)
+  chat_id_dict = content.json()
+  return chat_id_dict
+  
 def today_now():
   #Creates a file name with local india time
   India_local_time = datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
   time_now = India_local_time.strftime("%Y-%m-%d %H:%M:%S")
   return [time_now, India_local_time]
 
-
 def send_msg(text):
-  TOKEN = "6165499133:AAEvdzvgwhTi13lfDEMSIXjmiaJTp5fC3uI"
-  chat_id = "5752392578"
-  chat_id2 = "865024459"
+  TOKEN = "6063750922:AAHR7OsUtSXTr1_Y2z3wlZVd16fy6fDQr3M"
+  chat_id_dict = get_chatid()
   message = text
-  url1 = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
-  requests.get(url1)
-  url2 = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id2}&text={message}"
-  requests.get(url2)
+  for id in chat_id_dict.keys():
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={id}&text={message}"
+    requests.get(url)
   return
 
+def send_msg2(text):
+  TOKEN = "5950446560:AAEAK-FaOXNkMLTDVubcDDRcPc4YH2gSd7Y"
+  chat_id = ["5752392578"]
+  message = text
+  for id in chat_id:
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={id}&text={message}"
+    requests.get(url)
+  return
 
 def get_mmi():
   url = "https://www.tickertape.in/market-mood-index"
@@ -47,7 +58,7 @@ def weekend_sleep():
 def weekday_sleep():
   date_format_str = '%Y-%m-%d %H:%M:%S'
   now_time = datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
-  if(now_time.hour > 16):
+  if(now_time.hour >= 16):
     next_day = str((now_time + datetime.timedelta(days = 1)).date())
   else:
     next_day = str((now_time + datetime.timedelta(days = 0)).date())
@@ -62,47 +73,50 @@ def weekday_sleep():
 def main():
   run_no = 0
   new_mmi = get_mmi()
-  old_mmi = new_mmi
   in_id = 0
   out_id = 0
   while True:
-    while today_now()[1].hour < 16 and today_now()[1].hour >= 9:
-      print(f"run_no  {run_no}")
-      print(f"new mmi is {new_mmi}")
-      print(f"old mmi is {old_mmi}")
-      lower_bound = old_mmi - delta_change
-      print(f"Range lower bound is {lower_bound}")
-      upper_bound = old_mmi + delta_change
-      print(f"Range upper bound is {upper_bound}")
-
-      if (new_mmi <= target_value):
-        if (new_mmi <= lower_bound):
-          send_msg(u"\u2705"+f"mmi reduced more than {delta_change} and value is {new_mmi}")
-          old_mmi = new_mmi
-        elif (new_mmi >= upper_bound):
-          send_msg(u"\u274C"+f"&#10060 mmi increased more than {delta_change} and value is {new_mmi}")
-          old_mmi = new_mmi
-        elif (in_id == 0):
-          send_msg(u"\u2705"+f"mmi is lower than {target_value} and value is {new_mmi}")
-        in_id = 1
-        out_id = 0
-      elif (out_id == 0):
-        send_msg(u"\u274C"+f"mmi is more than {target_value} and value is {new_mmi}")
-        in_id = 0
-        out_id = 1
-        old_mmi = new_mmi
-
-      time.sleep(refresh_time * 60)
-      new_mmi = get_mmi()  #assigns the refreshed value
-      run_no = run_no + 1
     if ( (today_now()[1].weekday()== 4 and today_now()[1].hour>=16) or (today_now()[1].weekday() > 4 ) ):
-      send_msg(f"sleeping for {weekend_sleep()[0]} seconds now is weekend and starts at {weekend_sleep()[1]}")
+      send_msg2(f"sleeping for {weekend_sleep()[0]} seconds now is weekend and starts at {weekend_sleep()[1]}")
       time.sleep(float(weekend_sleep()[0]))
-      send_msg(f"bot started at {today_now()[0]}")
+      send_msg2(f"bot started at {today_now()[0]}")
     else:
-      send_msg(f"sleeping for {weekday_sleep()[0]} seconds now is weekday and starts at {weekday_sleep()[1]}")
+      send_msg2(f"sleeping for {weekday_sleep()[0]} seconds now is weekday and starts at {weekday_sleep()[1]}")
       time.sleep(float(weekday_sleep()[0]))
-      send_msg(f"bot started at {today_now()[0]}")
+      send_msg2(f"bot started at {today_now()[0]}")
+      
+    print(f"run_no  {run_no}")
+    print(f"new mmi is {new_mmi}")
+    print("old mmi is " + str(db["old_mmi"]))
+
+    
+    lower_bound = db[ "old_mmi" ] - delta_change
+    print(f"Range lower bound is {lower_bound}")
+    upper_bound = db[ "old_mmi" ] + delta_change
+    print(f"Range upper bound is {upper_bound}")
+
+    
+    if (new_mmi <= target_value):
+      if (new_mmi <= lower_bound):
+        send_msg(u"\u2705"+f"mmi reduced more than {delta_change} and value is {new_mmi}")
+        db[ "old_mmi" ] = new_mmi
+      elif (new_mmi >= upper_bound):
+        send_msg(u"\u274C"+f"mmi increased more than {delta_change} and value is {new_mmi}")
+        db[ "old_mmi" ] = new_mmi
+      elif (run_no != 0 and in_id == 0):
+        send_msg(u"\u2705"+f"mmi is lower than {target_value} and value is {new_mmi}")
+      in_id = 1
+      out_id = 0
+    elif (out_id == 0):
+      send_msg(u"\u274C"+f"mmi is more than {target_value} and value is {new_mmi}")
+      in_id = 0
+      out_id = 1
+      db[ "old_mmi" ] = new_mmi
+
+    time.sleep(refresh_time * 60)
+    new_mmi = get_mmi()  #assigns the refreshed value
+    run_no = run_no + 1
+
   return
 
 
